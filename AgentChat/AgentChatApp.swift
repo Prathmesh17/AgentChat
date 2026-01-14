138149b2a8779c18257798d1d3f5228c93844ae0
Prathmesh <prathmeshparteki@gmail.com>
2026-01-14 18:18:31 +0530

    Initial Commit

---
+++ AgentChat/AgentChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgentChatApp.swift
+//  AgentChat
+//
+//  Created by Prathmesh Parteki on 14/01/26.
+//
+
+import SwiftUI
+
+@main
+struct AgentChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AgentChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AgentChat
+//
+//  Created by Prathmesh Parteki on 14/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
